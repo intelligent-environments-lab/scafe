7b0b4301ab1b4d44e774855a2f0b500c75017fde
Stepan Ulyanin <steve.usuck@gmail.com>
2019-06-12 00:02:31 -0400

    some refactoring

--- ae.py
+++ ae.py
@@ -7,41 +7,23 @@ class Encoder(nn.Module):
 
     def __init__(self, latent_space_dim: int = 128):
         super(Encoder, self).__init__()
-
-        self.conv_1 = nn.Conv2d(in_channels=3,
-                                out_channels=32,
-                                kernel_size=3,
-                                stride=2,
-                                padding=1)
+        self.conv_1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=2, padding=1)
         self.bn_1 = nn.BatchNorm2d(num_features=32)
         self.prelu_1 = nn.PReLU()
 
-        self.conv_2 = nn.Conv2d(in_channels=32,
-                                out_channels=64,
-                                kernel_size=3,
-                                stride=2,
-                                padding=1)
+        self.conv_2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1)
         self.bn_2 = nn.BatchNorm2d(num_features=64)
         self.prelu_2 = nn.PReLU()
 
-        self.conv_3 = nn.Conv2d(in_channels=64,
-                                out_channels=128,
-                                kernel_size=3,
-                                stride=2,
-                                padding=1)
+        self.conv_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
         self.bn_3 = nn.BatchNorm2d(num_features=128)
         self.prelu_3 = nn.PReLU()
 
-        self.conv_4 = nn.Conv2d(in_channels=128,
-                                out_channels=256,
-                                kernel_size=3,
-                                stride=1,
-                                padding=0)
+        self.conv_4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=0)
         self.bn_4 = nn.BatchNorm2d(num_features=256)
         self.prelu_4 = nn.PReLU()
 
         self.fc_out = nn.Linear(in_features=256, out_features=latent_space_dim)
-
         self.gradients = None
 
     # hook for the gradients of the activations
@@ -62,6 +44,7 @@ class Encoder(nn.Module):
         x = self.fc_out(x)
         return x
 
+    # method for feature extraction
     def extract_features(self, x):
         x = self.prelu_1(self.bn_1(self.conv_1(x)))
         x = self.prelu_2(self.bn_2(self.conv_2(x)))
@@ -69,6 +52,7 @@ class Encoder(nn.Module):
         x = self.prelu_4(self.bn_4(self.conv_4(x)))
         return x
 
+    # method for activations extraction
     def get_activations(self, x):
         x = self.prelu_1(self.bn_1(self.conv_1(x)))
         x = self.prelu_2(self.bn_2(self.conv_2(x)))
@@ -83,35 +67,19 @@ class Decoder(nn.Module):
         self.fc_in = nn.Linear(in_features=latent_space_dim, out_features=256)
         self.prelu_in = nn.PReLU()
 
-        self.conv_1 = nn.ConvTranspose2d(in_channels=256,
-                                         out_channels=128,
-                                         kernel_size=3,
-                                         stride=1,
-                                         padding=0)
+        self.conv_1 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=0)
         self.bn_1 = nn.BatchNorm2d(num_features=128)
         self.prelu_1 = nn.PReLU()
 
-        self.conv_2 = nn.Conv2d(in_channels=128,
-                                out_channels=64,
-                                kernel_size=3,
-                                stride=1,
-                                padding=1)
+        self.conv_2 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.bn_2 = nn.BatchNorm2d(num_features=64)
         self.prelu_2 = nn.PReLU()
 
-        self.conv_3 = nn.Conv2d(in_channels=64,
-                                out_channels=32,
-                                kernel_size=3,
-                                stride=1,
-                                padding=1)
+        self.conv_3 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.bn_3 = nn.BatchNorm2d(num_features=32)
         self.prelu_3 = nn.PReLU()
 
-        self.conv_4 = nn.Conv2d(in_channels=32,
-                                out_channels=3,
-                                kernel_size=3,
-                                stride=1,
-                                padding=1)
+        self.conv_4 = nn.Conv2d(in_channels=32, out_channels=3, kernel_size=3, stride=1, padding=1)
         self.bn_4 = nn.BatchNorm2d(num_features=3)
         self.prelu_4 = nn.PReLU()
 
